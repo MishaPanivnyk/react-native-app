024348ea39d4022623e70f321b6eb35e26ffc5f2
Misha Panivnyk <panivnykm@gmail.com>
2023-03-18 18:01:50 +0200

    add Screens

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from "react-native";
+import { useFonts } from "expo-font";
+import { colors } from "./helpers/colors";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "SFP-Light": require("./assets/fonts/SFProDisplay-Light.ttf"),
+    "SFP-Regular": require("./assets/fonts/SFProDisplay-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Misha Panivnyk</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.bgImage}
+          source={require("./assets/images/bg_image.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={-100}
+          >
+            <RegistrationScreen />
+
+            {/* <LoginScreen /> */}
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: colors.white,
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
---
+++ helpers/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  blue: "#1B4371",
+  orange: "#FF6C00",
+  background: "#F6F6F6",
+  white: "#fff",
+  black: "#212121",
+  textColor: "#BDBDBD",
+  borderColor: "#E8E8E8",
+  bgWithOpacity: "#ffffff4d",
+  iconColor: "#212121cc",
+};
